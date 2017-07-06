3e26c116dc6607645185397b07892be99de6e309
ashleymarkfletcher <ashleymarkfletcher@gmail.com>
2017-07-06 22:10:26 +0100

    started adding score entry

--- public/js/index.js
+++ public/js/index.js
@@ -17,8 +17,6 @@ function setup() {
   frameRate(10)
   // snake = new Snake()
   noStroke()
-
-
 }
 
 function draw() {
@@ -82,15 +80,31 @@ function endGame(){
 	textSize(32);
 	var msg = 'Game Over';
 	var score = 'Your Score is ' + snake.tail.length;
+  var nameMessage = 'Enter Your Name';
+
 	msgWidth = textWidth(msg);
 	scoreWidth = textWidth(score);
 	fill(snakeColour);
 	text(msg, (width - msgWidth)/2, height/2 - 40);
 	text(score, (width - scoreWidth)/2, height/2);
-	startBtn = createButton('Restart Game');
+	var startBtn = createButton('Restart Game');
 	startBtn.position(width/2 - startBtn.width/2, height/2 + 40);
 	startBtn.mousePressed(startScreen);
+
+  text(nameMessage, width/2, height/2 + 70);
+
+  var inp = createInput('');
+  inp.input(myInputEvent);
+  inp.position(width/2 - inp.width/2, height/2 + 100);
+
+  var saveName = createButton('Save');
+  saveName.position(width/2 - saveName.width/2, height/2 + 130);
+  saveName.mousePressed(startScreen);
 	noLoop();
+}
+
+function myInputEvent(){
+  console.log('you are typing: ', this.value());
 }
 
 // pick the random location for food
