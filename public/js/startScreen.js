07cab02c6b75f03b2aa5a779486c8794cad530c6
ashley fletcher <ashley.fletcher@solocontrol.co.uk>
2017-07-09 13:34:01 +0100

    added high scrore page

--- public/js/startScreen.js
+++ public/js/startScreen.js
@@ -7,9 +7,47 @@ function startScreen() {
   fill(snakeColour);
   nameWidth = textWidth(name);
   text(name, (width - nameWidth)/2, height/2 - 40);
-  
-  startBtn = createButton('Start Game');
+
+  var startBtn = createButton('Start Game');
   startBtn.position(width/2 - startBtn.width/2, height/2);
   startBtn.mousePressed(initGame);
+
+  var highscoreBtn = createButton('High Scores');
+  highscoreBtn.position(width/2 - startBtn.width/2, height/2+100);
+  highscoreBtn.mousePressed(highscore);
   noLoop();
+}
+
+function highscore() {
+  removeElements()
+  background(screenColour);
+  var name = 'HIGH SCORES';
+  textSize(50);
+  fill(snakeColour);
+  nameWidth = textWidth(name);
+  text(name, (width - nameWidth)/2, height/2 - 100);
+
+  var backBtn = createButton('back');
+  backBtn.position(width/2 - backBtn.width/2, height/2+100);
+  backBtn.mousePressed(startScreen);
+
+  getScores()
+
+  noLoop();
+}
+
+function getScores() {
+
+  var ref = db.ref("scores").orderByChild("score").limitToLast(2)
+  ref.once("value")
+    .then(function(snapshot) {
+      console.log('val', snapshot.val());
+
+      var scores = snapshot.val()
+      var scoresArray = Object.keys(scores).map((key) => {
+        return scores[key]
+      }).reverse()
+
+      console.log('arr', scoresArray);
+    });
 }
